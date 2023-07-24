d5eeac7580b4a69843ebe53ff1f1e9c1c22cb7cc
juliaka-meet <juliakagan109@gmail.com>
2023-07-24 17:29:50 +0300

    j7yh
    yrhun

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
-
+from datetime import date, datetime
 
 config = {
   "apiKey": "AIzaSyB7gX49YikSHgKlxdpIL6PbOEINiCnQM-A",
@@ -62,20 +62,40 @@ def add_tweet():
     if request.method == 'POST':
         title = request.form["title"]
         text = request.form["text"]
+        
+        try: 
+            current_date = date.today()
+            current_date = current_date.strftime("%B %d, %Y")
+            now = datetime.now()
+            dt_string = now.strftime("%H:%M")
+            UID = login_session['user']['localId']
+            tweet = {'title':title,'text':text, 'date':current_date, 'hour':dt_string, 'likes':0, 'uid':UID}
+            db.child('Tweets').push(tweet)
+            return redirect(url_for('all_tweets'))
+        except:
+            return render_template("add_tweet.html")
 
-    try: 
-        tweet = {'title':title,'text':text, 'uid':UID}
-        db.child('Tweets').push(tweet)
     return render_template("add_tweet.html")
 
 
-@app.route('/all_tweets', methods=['GET'])
+@app.route('/all_tweets')
 def all_tweets():
     try: 
         tweets = db.child('Tweets').get().val()
-        return redirect(url_for('all_tweets.html'), tweets=tweets)
+        return render_template('all_tweets.html', tweets=tweets)
     except:
         return render_template("add_tweet.html")
 
+
+app.route('/add_like')
+def add_like():
+    try: 
+        print(Tweet)
+        Tweet["likes"] += 1
+        db.child("Tweets").child(Tweet).update(tweet)
+        tweets = db.child('Tweets').get().val()
+        return render_template("all_tweets.html", tweets=tweets)
+    except: 
+        return render_template('all_tweets.html')
 if __name__ == '__main__':
     app.run(debug=True)
